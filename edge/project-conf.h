4a81af4d26876aa14b3f7a6a91ab8877cedd547d
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-05-20 10:47:49 +0100

    node: edge: Remove reduced resources from project-conf.h while we use 32K of memory

--- edge/project-conf.h
+++ edge/project-conf.h
@@ -6,10 +6,10 @@
 
 #define LOG_CONF_LEVEL_COAP LOG_LEVEL_WARN
 
-#define UIP_CONF_UDP_CONNS 4
-#define QUEUEBUF_CONF_NUM 4
-#define NBR_TABLE_CONF_MAX_NEIGHBORS 6
-#define NETSTACK_MAX_ROUTE_ENTRIES 6
+//#define UIP_CONF_UDP_CONNS 4
+//#define QUEUEBUF_CONF_NUM 4
+//#define NBR_TABLE_CONF_MAX_NEIGHBORS 6
+//#define NETSTACK_MAX_ROUTE_ENTRIES 6
 
 // This is the address of the observer node connected to the border router
 #define MQTT_CLIENT_CONF_BROKER_IP_ADDR "fd00::1"
