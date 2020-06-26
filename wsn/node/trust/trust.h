4a47254243e58ab45bfe5d4db438e951f02d3d9f
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-06-26 11:37:52 +0100

    trust: Increase maximum trust payload size

--- wsn/node/trust/trust.h
+++ wsn/node/trust/trust.h
@@ -2,7 +2,9 @@
 
 #include "edge-info.h"
 
+#include "keys.h"
+
 #define TRUST_COAP_URI "trust"
-#define MAX_TRUST_PAYLOAD (128 + (sizeof(uint32_t) * 8 * 2))
+#define MAX_TRUST_PAYLOAD (COAP_MAX_CHUNK_SIZE - DTLS_EC_SIG_SIZE)
 
 edge_resource_t* choose_edge(const char* capability_name);
