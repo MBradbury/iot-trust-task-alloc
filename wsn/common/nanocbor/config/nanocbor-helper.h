4e314fe97f55c96f3dd772d467cad4ccbbe2c5d3
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-06-29 20:55:27 +0100

    nanocbor: Improve error message when failing to parse

--- wsn/common/nanocbor/config/nanocbor-helper.h
+++ wsn/common/nanocbor/config/nanocbor-helper.h
@@ -7,10 +7,10 @@
 
 #define NANOCBOR_CHECK(expr) \
     do { \
-        int result = (expr); \
+        const int result = (expr); \
         if (result < 0) \
         { \
-            LOG_ERR("Failed '" PP_STR(expr) "' on line " PP_STR(__LINE__) " in file " __FILE__ " (result=%d)\n", result); \
+            LOG_ERR("Failed '" PP_STR(expr) "' at " __FILE__ ":" PP_STR(__LINE__) " (result=%d)\n", result); \
             return result; \
         } \
     } while (0)
@@ -21,7 +21,7 @@
         NANOCBOR_CHECK(nanocbor_get_bstr(dec, (const uint8_t**)obj, &len)); \
         if (len != sizeof(**obj)) \
         { \
-            LOG_ERR("Failed NANOCBOR_GET_OBJECT on line " PP_STR(__LINE__) " in file " __FILE__ " (%zu != %zu)\n", len, sizeof(**obj)); \
+            LOG_ERR("Failed NANOCBOR_GET_OBJECT at " __FILE__ ":" PP_STR(__LINE__) " (%zu != %zu)\n", len, sizeof(**obj)); \
             return -1; \
         } \
     } while (0)
