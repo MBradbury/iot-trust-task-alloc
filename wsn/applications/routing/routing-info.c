3cb6b65ff9f7f2ebf45be7bb73aa0a8a382f8019
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-07-06 13:12:14 +0100

    applications/routing: Update trust metric weights

--- wsn/applications/routing/routing-info.c
+++ wsn/applications/routing/routing-info.c
@@ -2,7 +2,9 @@
 #include "trust-models.h"
 
 static const trust_weight_t weights[] = {
-    { TRUST_METRIC_TASK_SUBMISSION, 1.0f },
+    { TRUST_METRIC_TASK_SUBMISSION, 0.33333f },
+    { TRUST_METRIC_TASK_RESULT, 0.33333f },
+    { TRUST_METRIC_RESULT_QUALITY, 0.33333f },
 };
 
 static trust_weights_t weights_info = {
