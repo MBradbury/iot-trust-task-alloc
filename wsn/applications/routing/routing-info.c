ce67a95e589441fb7881fb8420e3b922d29734d5
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-07-06 14:44:16 +0100

    applications/routing: Fix warning about trust weights not being 1

--- wsn/applications/routing/routing-info.c
+++ wsn/applications/routing/routing-info.c
@@ -2,9 +2,9 @@
 #include "trust-models.h"
 
 static const trust_weight_t weights[] = {
-    { TRUST_METRIC_TASK_SUBMISSION, 0.33333f },
-    { TRUST_METRIC_TASK_RESULT, 0.33333f },
-    { TRUST_METRIC_RESULT_QUALITY, 0.33333f },
+    { TRUST_METRIC_TASK_SUBMISSION, 1f/3f },
+    { TRUST_METRIC_TASK_RESULT,     1f/3f },
+    { TRUST_METRIC_RESULT_QUALITY,  1f/3f },
 };
 
 static trust_weights_t weights_info = {
