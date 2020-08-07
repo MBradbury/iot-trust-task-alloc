489bed82cabfaebfbef3259ef9f1bcee0477cb2b
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-08-07 17:30:08 +0100

    Update test runner scripts

--- tests/run/root.sh
+++ tests/run/root.sh
@@ -6,8 +6,11 @@ pkill -9 python3
 # Remove logs
 rm -rf ~/iot-trust-task-alloc/logs
 
-rm nohup.out
+rm -f nohup.out
 
 nohup ~/iot-trust-task-alloc/tools/run/root.py &
+
+# Wait for nohup.out to be created
+sleep 1
 
 tail -f nohup.out
--- tests/run/wsn.sh
+++ tests/run/wsn.sh
@@ -6,8 +6,11 @@ pkill -9 python3
 # Remove logs
 rm -rf ~/iot-trust-task-alloc/logs
 
-rm nohup.out
+rm -f nohup.out
 
 nohup ~/iot-trust-task-alloc/tools/run/wsn.py &
+
+# Wait for nohup.out to be created
+sleep 1
 
 tail -f nohup.out
