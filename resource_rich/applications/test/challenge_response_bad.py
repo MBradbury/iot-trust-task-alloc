03b5a5d3ffae09e3b2fa694c19cb9cc273c8b753
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-08-08 18:17:06 +0100

    applications/test: Add a bad challenge response application that will potentially do the wrong thing

---
+++ resource_rich/applications/test/challenge_response_bad.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import logging
+import random
+import asyncio
+
+import applications.challenge_response as good_cr
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("app-challenge-response-bad")
+logger.setLevel(logging.DEBUG)
+
+MISBEHAVE_CHOICES = ["bad-response", "no-response"]
+
+class ChallengeResponseClient(good_cr.ChallengeResponseClient):
+    def __init__(self, approach, duration):
+        super().__init__()
+
+        self.approach = approach
+        self.duration = duration
+
+        # Start off being good
+        self.is_bad = False
+
+        if self.duration == float('inf'):
+            # Always bad
+            self.is_bad = True
+        else:
+            asyncio.create_task(self._periodic())
+
+        logger.debug(f"Currently is_bad={not self.is_bad} changing to opposite")
+
+    async def _periodic(self):
+        while True:
+            await asyncio.sleep(self.duration)
+
+            logger.debug(f"Currently is_bad={self.is_bad} changing to opposite")
+
+            self.is_bad = not self.is_bad
+
+
+    async def _send_result(self, dest, message_response):
+        if self.is_bad:
+            await self._write_task_stats()
+
+            if self.approach == "random":
+                selected_approach = random.choice(MISBEHAVE_CHOICES)
+            else:
+                selected_approach = self.approach
+
+            logger.debug(f"Currently good, so behaving incorrectly with {selected_approach}")
+
+            # Instead of sending a result, we pick one of two options
+            # 1. Send a bad response
+            # 2. Don't send any response
+
+            if self.approach == "bad-response":
+                # A bad message response
+                message_response = (b'', 0)
+                await self._write_task_result(dest, message_response)
+
+            elif selected_approach == "no-response":
+                # Nothing to do
+                pass
+
+            else:
+                logger.error(f"Unknown misbehaviour {selected_approach}")
+
+        else:
+            logger.debug(f"Currently good, so behaving correctly")
+            await super()._send_result(dest, message_response)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='challenge-reponse always bad')
+    parser.add_argument('--approach', type=str, choices=MISBEHAVE_CHOICES + ["random"], help='How will this application misbehave')
+    parser.add_argument('--duration', type=float, help='How long will this application misbehave for in seconds')
+    args = parser.parse_args()
+
+    client = ChallengeResponseClient(args.approach, args.duration)
+
+    client_common.main("cr", client)
