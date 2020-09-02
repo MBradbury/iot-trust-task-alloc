ecaa2ee7d0a5e805606943496f9fd643d961e6b1
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-09-02 12:24:20 +0100

    common/nanocbor: Use CC_STRINGIFY

--- wsn/common/nanocbor/config/nanocbor-helper.h
+++ wsn/common/nanocbor/config/nanocbor-helper.h
@@ -2,17 +2,15 @@
 
 #include "nanocbor/nanocbor.h"
 
+#include "cc.h"
 #include "uip.h"
-
-#define PP_STR(x) PP_STR_(x)
-#define PP_STR_(x) #x
 
 #define NANOCBOR_CHECK(expr) \
     do { \
         const int result = (expr); \
         if (result < 0) \
         { \
-            LOG_ERR("Failed '" PP_STR(expr) "' at " __FILE__ ":" PP_STR(__LINE__) " (result=%d)\n", result); \
+            LOG_ERR("Failed '" CC_STRINGIFY(expr) "' at " __FILE__ ":" CC_STRINGIFY(__LINE__) " (result=%d)\n", result); \
             return result; \
         } \
     } while (0)
@@ -23,7 +21,7 @@
         NANOCBOR_CHECK(nanocbor_get_bstr(dec, (const uint8_t**)obj, &len)); \
         if (len != sizeof(**obj)) \
         { \
-            LOG_ERR("Failed NANOCBOR_GET_OBJECT at " __FILE__ ":" PP_STR(__LINE__) " (%zu != %zu)\n", len, sizeof(**obj)); \
+            LOG_ERR("Failed NANOCBOR_GET_OBJECT at " __FILE__ ":" CC_STRINGIFY(__LINE__) " (%zu != %zu)\n", len, sizeof(**obj)); \
             return -1; \
         } \
     } while (0)
