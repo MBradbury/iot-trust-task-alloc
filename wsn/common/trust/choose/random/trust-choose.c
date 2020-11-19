8ac75d426cd62fee2a79e101c081ca9094462704
Matthew Bradbury <M.Bradbury@warwick.ac.uk>
2020-11-19 09:18:53 +0000

    trust/choose: Add the ability to randomly choose which Edge node to submit tasks to

---
+++ wsn/common/trust/choose/random/trust-choose.c
@@ -0,0 +1,41 @@
+#include "trust-choose.h"
+#include "trust-model.h"
+#include "edge-info.h"
+#include "random-helpers.h"
+#include "os/sys/log.h"
+/*-------------------------------------------------------------------------------------------------------------------*/
+#define LOG_MODULE "trust-random"
+#ifdef TRUST_MODEL_LOG_LEVEL
+#define LOG_LEVEL TRUST_MODEL_LOG_LEVEL
+#else
+#define LOG_LEVEL LOG_LEVEL_NONE
+#endif
+/*-------------------------------------------------------------------------------------------------------------------*/
+// Use the a random edge node we are aware of that supports
+// the provided capability
+edge_resource_t* choose_edge(const char* capability_name)
+{
+    edge_resource_t* candidates[NUM_EDGE_RESOURCES];
+    uint16_t candidates_len = 0;
+
+    for (edge_resource_t* iter = edge_info_iter(); iter != NULL; iter = edge_info_next(iter))
+    {
+        edge_capability_t* capability = edge_info_capability_find(iter, capability_name);
+        if (capability != NULL)
+        {
+            candidates[candidates_len] = iter;
+            candidates_len++;
+        }
+    }
+
+    // No valid options
+    if (candidates_len == 0)
+    {
+        return NULL;
+    }
+
+    uint16_t idx = random_in_range_unbiased(0, candidates_len-1);
+
+    return candidates[idx];
+}
+/*-------------------------------------------------------------------------------------------------------------------*/
