274cf300365533b682d905269975f4e2332e22d0
Matthew Bradbury <matt-bradbury@live.co.uk>
2022-02-03 16:24:00 +0000

    Simplify implementation of nrf flashing

--- tools/deploy/flash_backend/nRF52840_flash.py
+++ tools/deploy/flash_backend/nRF52840_flash.py
@@ -4,60 +4,19 @@ import subprocess
 from pathlib import Path
 
 import pynrfjprog.HighLevel
-import pynrfjprog.APIError
-
-JLINK_DIR = Path("/opt/SEGGER/JLink")
-JLINK_EXE = JLINK_DIR / "JLinkExe"
-
-def write_flash_jlink(target):
-
-    # 1. Load binary
-    #    https://wiki.segger.com/J-Link_Commander#LoadFile
-    # 2. Issue reset via "ResetX" with a delay of 1000ms after reset
-    #    https://wiki.segger.com/J-Link_Commander#Reset
-    # 3. Start CPU via "Go"
-    #    https://wiki.segger.com/J-Link_Commander#Go
-    # 4-5 Show information
-    # 6. Exit
-
-    """
-    Syntax: RSetType <type>
-
-        Types:
-        0 = NORMAL (Resets core & peripherals via SYSRESETREQ & VECTRESET bit.)
-        1 = CORE (Resets the core only, not peripherals.)
-        2 = RESETPIN (Resets core & peripherals using RESET pin.)
-        3 = CONNECT UNDER RESET (Halts RESET Pin low when connecting to core)
-        4 = HALT AFTER BTL (Resets core & peripherals, halts CPU after the bootloader.)
-        5 = HALT BEFORE BTL (Resets core & peripherals, halts CPU before the bootloader.)
-        6 = KINETIS (Reset via strategy NORMAL. Watchdog will be disabled after reset)
-        7 = ADI HALT AFTER KERNEL (Resets core & peripherals, halts CPU after the ADI kernel.)
-        8 = CORE AND PERIPHERALS (Resets core & peripherals via SYSRESETREQ bit only.)
-        9 = LPC1200 (Reset via strategy normal. Watchdog will be disabled after reset)
-        10 = S3FN60D (Reset via strategy normal. Watchdog will be disabled after reset)
-        11 = LPC11A (Resets core & peripherals. Performs some special handling which is needed by some LPC11A bootloaders.)
-        12 = Halter after BTL using WP (Resets core & peripherals via SYSRESETREQ. Halts CPU after BTL using WP.)
-    """
-
-    with open("flash.jlink", "w") as f:
-        print(f"""LoadFile {target} 0x0
-RSetType 0
-ResetX 1000
-Go
-ShowFWInfo
-ShowHWStatus
-Exit""", file=f)
+from pynrfjprog.Parameters import ProgramOptions, VerifyAction, EraseAction, ResetAction
 
 def get_serial_number_for_mote(mote: str) -> int:
     with pynrfjprog.HighLevel.API() as api:
         for node_id in api.get_connected_probes():
             with pynrfjprog.HighLevel.DebugProbe(api, node_id) as probe:
-                probe_info = probe.get_probe_info()
                 device_info = probe.get_device_info()
 
                 # Make sure to only allow flashing to appropriate devices
                 if not device_info.device_type.name.startswith("NRF52840"):
                     continue
+
+                probe_info = probe.get_probe_info()
 
                 if mote in [com_port.path for com_port in probe_info.com_ports]:
                     return node_id
@@ -73,18 +32,18 @@ def flash_nrf52840(filename, mote=None, serial_number=None):
         assert serial_number is None
         serial_number = get_serial_number_for_mote(mote)
 
-    opts = {
-        "-Device": "nRF52840_xxAA",
-        "-if": "swd",
-        "-speed": 2000,
-        "-SelectEmuBySN": serial_number,
-    }
+    serial_number = int(serial_number)
 
-    opts_str = " ".join(f"{k} {v}" for (k, v) in opts.items())
+    program_options = ProgramOptions(
+        verify=VerifyAction.VERIFY_READ,
+        erase_action=EraseAction.ERASE_ALL,
+        qspi_erase_action=EraseAction.ERASE_NONE,
+        reset=ResetAction.RESET_SYSTEM
+    )
 
-    write_flash_jlink(filename)
-
-    subprocess.run(f"{JLINK_EXE} {opts_str} -CommanderScript flash.jlink", shell=True, check=True)
+    with pynrfjprog.HighLevel.API() as api:
+        with pynrfjprog.HighLevel.DebugProbe(api, serial_number) as probe:
+            probe.program(filename, program_options)
 
 if __name__ == "__main__":
     import argparse
