d13ed060c5434f5848a300053758be21b21a6c60
Matthew Bradbury <matt-bradbury@live.co.uk>
2022-04-08 09:42:47 +0100

    tests/scenarios/throughput-periodically-slow: Add playbook

--- tests/run/profile.sh
+++ tests/run/profile.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+INTERACTIVE=1
+
+while getopts "d" flag
+do
+    case "${flag}" in
+        d) INTERACTIVE=0;;
+    esac
+done
+
 # Stop anything currently running
 sudo pkill python3
 
 # Remove logs
 rm -rf logs
+mkdir logs
 
-rm -f nohup.out
+nohup python3 -m tools.run.profile \
+    </dev/null >logs/$(hostname).nohup.out 2>&1 &
 
-nohup python3 -m tools.run.profile &
+if [[ $INTERACTIVE == 1 ]]
+then
+    # Wait for nohup.out to be created
+    sleep 1
 
-# Wait for nohup.out to be created
-sleep 1
-
-tail -f nohup.out
+    tail -f logs/$(hostname).nohup.out
+fi
--- tests/scenarios/throughput-periodically-slow/bad_edge.sh
+++ tests/scenarios/throughput-periodically-slow/bad_edge.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
+INTERACTIVE=1
+
+while getopts "d" flag
+do
+    case "${flag}" in
+        d) INTERACTIVE=0;;
+    esac
+done
+
 # Stop anything currently running
 sudo pkill python3
 
 # Remove logs
 rm -rf logs
-
-rm -f nohup.out
+mkdir logs
 
 # Cannot set negative niceness without running at higher privilege, so just use higher positive numbers to indicate
 # a lower priority relative to each application.
 
-nohup python3 -m tools.run.edge --application monitoring 2 --application bad_routing 0 " --approach slow --duration 300 --slow-wait 1" --application challenge_response 1 &
+nohup python3 -m tools.run.edge \
+    --application monitoring 2 \
+    --application bad_routing 0 " --approach slow --duration 300 --slow-wait 1" \
+    --application challenge_response 1 \
+    </dev/null >logs/$(hostname).nohup.out 2>&1 &
 
-# Wait for nohup.out to be created
-sleep 1
+if [[ $INTERACTIVE == 1 ]]
+then
+    # Wait for nohup.out to be created
+    sleep 1
 
-tail -f nohup.out
+    tail -f logs/$(hostname).nohup.out
+fi
+
