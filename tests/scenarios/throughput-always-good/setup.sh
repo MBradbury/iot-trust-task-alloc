47072c47318cf5186436ca6a5127ec5a846755bc
Matthew Bradbury <matt-bradbury@live.co.uk>
2023-02-15 14:01:49 +0000

    throughput-always-good: Define parameters in setup.sh

--- tests/scenarios/throughput-always-good/setup.sh
+++ tests/scenarios/throughput-always-good/setup.sh
@@ -5,5 +5,5 @@ python3 -m tools.setup throughput_pr badlisted_banded  \
     --deploy ansible \
     --with-pcap \
     --defines APPLICATIONS_MONITOR_THROUGHPUT 1 \
-    --defines TRUST_MODEL_PERIODIC_EDGE_PING 1 \
-    --defines TRUST_MODEL_PERIODIC_EDGE_PING_INTERVAL 10
+    --defines EXPECTED_TIME_THROUGHPUT_BAD 10 \
+    --defines EXPECTED_TIME_THROUGHPUT_BAD_TO_GOOD_PR 0.6
